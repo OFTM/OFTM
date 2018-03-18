0a69af6471eeefa0e105d37c338c06c4fbc7b92a
Daniel Bacher <danielbacher@phill93.de>
2018-03-18 15:43:01 +0100

    add tournament model

---
+++ database/migrations/2018_03_18_143945_create_tournaments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTournamentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tournaments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('typ')->unsigned();
+            $table->integer('weapon')->unsigned();
+            $table->integer('ageclass')->unsigned();
+            $table->integer('sex_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tournaments');
+    }
+}
