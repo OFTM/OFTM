70b89bf961f579ae4f6d952306d1056e0bcb508d
Daniel Bacher <mail@phill93.de>
2018-04-18 13:25:47 +0200

    define api endpoints for fencer reading

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('fencers', function () {
+    return App\fencer::all()->load('person', 'person.sex');
+});
+
+Route::get('fencer/{id}', function ($id) {
+    return App\fencer::find($id)->load('person', 'person.sex');
+});
