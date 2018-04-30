e09733afd684ac528ad914879914bc43a13b060e
Daniel Bacher <mail@phill93.de>
2018-04-30 16:41:02 +0200

    move content section out of head

--- resources/views/menu/sidebar.blade.php
+++ resources/views/menu/sidebar.blade.php
@@ -17,7 +17,7 @@
                 <button class="btn btn-outline-dark toggle-button"><i id="slideout-button"
                                                                       class="fa fa-angle-right"></i></button>
             </div>
-            @yield('content')
         </header>
+        @yield('content')
     </main>
 </Slideout>
