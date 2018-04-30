7773d9f98253459532536fff31e72b252607541f
Daniel Bacher <mail@phill93.de>
2018-04-30 16:45:23 +0200

    fix paths to js and css files

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-    <link rel="stylesheet" type="text/css" href="css/app.css">
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
 
 </head>
 <body>
 <div id="app">
     @include('menu.sidebar')
 </div>
-<script src="js/app.js"></script>
+<script src="/js/app.js"></script>
 </body>
 </html>
