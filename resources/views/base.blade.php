21dbce8d1f8ed31b36773d2d24cbcfd3bccf459b
Daniel Bacher <mail@phill93.de>
2018-05-08 16:46:15 +0200

    add csrf token

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <link rel="stylesheet" type="text/css" href="/css/app.css">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
 <div id="app">
