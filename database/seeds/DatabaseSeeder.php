331f2500d71aeb995f273e0e212d6e558eeaee77
Daniel Bacher <mail@phill93.de>
2018-06-01 14:50:33 +0200

    create multiple seeders
    for:
    - referees
    - participants
    - combats

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(FencerSeeder::class);
+        $this->call(RefereeSeeder::class);
         $this->call(TournamentSeeder::class);
     }
 }
--- database/seeds/TournamentSeeder.php
+++ database/seeds/TournamentSeeder.php
@@ -12,5 +12,14 @@ class TournamentSeeder extends Seeder
     public function run()
     {
         factory(\App\tournament::class, 10)->create();
+        for ($i = 1; $i <= 200; $i++) {
+            $f = \App\fencer::all()->random(1)->first->get();
+            $t = \App\tournament::all()->random(1)->first->get();
+            $p = new \App\participant();
+            $p->fencer()->associate($f);
+            $p->tournament()->associate($t);
+            $p->save();
+        }
+        factory(\App\combat::class, 50)->create();
     }
 }
