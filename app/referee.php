6f80414e8c98d348e2854626092232205b7d2d0e
Daniel Bacher <mail@phill93.de>
2018-06-01 14:49:23 +0200

    update referee model

--- app/referee.php
+++ app/referee.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class referee extends Model
 {
-    //
+    public function person() {
+        return $this->belongsTo('App\people', 'people_id');
+    }
 }
