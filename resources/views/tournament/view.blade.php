a8bda84fb3d9b3f1ed823162c59e811091adc400
Daniel Bacher <mail@phill93.de>
2018-06-19 17:05:49 +0200

    add visual representation of combat state

--- resources/views/tournament/view.blade.php
+++ resources/views/tournament/view.blade.php
@@ -100,11 +100,29 @@
                         @foreach($tournament->combats as $combat)
                             @if($combat->round == $i)
                                 <tr>
-                                    <td>{{ $combat->participant1->fencer->person->forename }} {{ $combat->participant1->fencer->person->surname }}</td>
-                                    <td class="text-center">{{ $combat->hits1 }}</td>
+                                    @if($combat->hits1 > $combat->hits2)
+                                        <td class="table-success">{{ $combat->participant1->fencer->person->forename }} {{ $combat->participant1->fencer->person->surname }}</td>
+                                        <td class="text-center table-success">{{ $combat->hits1 }}</td>
+                                    @elseif ($combat->hits1 == $combat->hits2)
+                                        <td class="table-primary">{{ $combat->participant1->fencer->person->forename }} {{ $combat->participant1->fencer->person->surname }}</td>
+                                        <td class="text-center table-primary">{{ $combat->hits1 }}</td>
+                                    @else
+                                        <td>{{ $combat->participant1->fencer->person->forename }} {{ $combat->participant1->fencer->person->surname }}</td>
+                                        <td class="text-center">{{ $combat->hits1 }}</td>
+                                    @endif
+
                                     <td class="text-center">vs.</td>
-                                    <td class="text-center">{{ $combat->hits2 }}</td>
-                                    <td class="text-right">{{ $combat->participant2->fencer->person->forename }} {{ $combat->participant2->fencer->person->surname }}</td>
+
+                                    @if($combat->hits1 < $combat->hits2)
+                                        <td class="text-center table-success">{{ $combat->hits2 }}</td>
+                                        <td class="table-success text-right">{{ $combat->participant2->fencer->person->forename }} {{ $combat->participant2->fencer->person->surname }}</td>
+                                    @elseif ($combat->hits1 == $combat->hits2)
+                                        <td class="text-center table-primary">{{ $combat->hits2 }}</td>
+                                        <td class="table-primary text-right">{{ $combat->participant2->fencer->person->forename }} {{ $combat->participant2->fencer->person->surname }}</td>
+                                    @else
+                                        <td class="text-center">{{ $combat->hits2 }}</td>
+                                        <td class="text-right">{{ $combat->participant2->fencer->person->forename }} {{ $combat->participant2->fencer->person->surname }}</td>
+                                    @endif
                                 </tr>
                             @endif
                         @endforeach
