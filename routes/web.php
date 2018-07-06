2a71fe99f60a7680c0a42749a2ba5e0a52a8376d
Daniel Bacher <danielbacher@phill93.de>
2018-07-06 16:07:44 +0200

    add create view for adding combats

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::resource('fencers', 'FencerController');
 Route::get('tournament/{tournament}/participants/edit', array('uses' => 'TournamentController@participants_edit'))->name('tournament.participants_edit');
 Route::put('tournament/{tournament}/participants', array('uses' => 'TournamentController@participants_store'))->name('tournament.participants_store');
 Route::get('tournament/{tournament}/combats/create', array('uses' => 'TournamentController@combats_create'))->name('tournament.combats_create');
-Route::put('tournament/{tournament}/combats', array('uses' => 'TournamentController@combats_store'))->name('tournament.combats_store');
+Route::post('tournament/{tournament}/combats', array('uses' => 'TournamentController@combats_store'))->name('tournament.combats_store');
 Route::post('tournament/{tournament}/end_round', array('uses' => 'TournamentController@end_round'))->name('tournament.end_round');
 Route::resource('tournament', 'TournamentController');
