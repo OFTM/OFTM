49a1f85b53391a52b0b36a830e5fe481cbd9cf58
Daniel Bacher <danielbacher@phill93.de>
2018-07-08 13:53:54 +0200

    add route for combat update

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,6 @@ Route::get('tournament/{tournament}/participants/edit', array('uses' => 'Tournam
 Route::put('tournament/{tournament}/participants', array('uses' => 'TournamentController@participants_store'))->name('tournament.participants_store');
 Route::get('tournament/{tournament}/combats/create', array('uses' => 'TournamentController@combats_create'))->name('tournament.combats_create');
 Route::post('tournament/{tournament}/combats', array('uses' => 'TournamentController@combats_store'))->name('tournament.combats_store');
+Route::put('tournament/{tournament}/combats/{combat}', array('uses' => 'TournamentController@combats_update'))->name('tournament.combats_update');
 Route::post('tournament/{tournament}/end_round', array('uses' => 'TournamentController@end_round'))->name('tournament.end_round');
 Route::resource('tournament', 'TournamentController');
