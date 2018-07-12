2dbdd575dca3efe5d3eb80b6ac3b5397f7dcb716
Daniel Bacher <danielbacher@phill93.de>
2018-07-12 17:47:39 +0200

    display more informations on the beamer

--- app/Helpers/GenerateRanking.php
+++ app/Helpers/GenerateRanking.php
@@ -14,11 +14,14 @@ if (!function_exists('GenerateRanking')) {
     {
         $rank = [];
         foreach ($tournament->participants as $participant) {
-            $rank[$participant->id] = ['id' => $participant->id, 'wins' => 0, 'assigned' => False, 'given_hits' => 0, 'index' => 0, 'participant' => $participant];
+            $rank[$participant->id] = ['id' => $participant->id, 'wins' => 0, 'assigned' => False, 'given_hits' => 0, 'recv_hits' => 0, 'index' => 0, 'participant' => $participant];
         }
         foreach ($tournament->combats as $combat) {
             $rank[$combat->participant1_id]['given_hits'] += $combat->hits1;
             $rank[$combat->participant2_id]['given_hits'] += $combat->hits2;
+
+            $rank[$combat->participant1_id]['recv_hits'] += $combat->hits2;
+            $rank[$combat->participant2_id]['recv_hits'] += $combat->hits1;
 
             $rank[$combat->participant1_id]['index'] += $combat->hits1 - $combat->hits2;
             $rank[$combat->participant2_id]['index'] += $combat->hits2 - $combat->hits1;
