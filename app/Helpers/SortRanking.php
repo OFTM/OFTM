a114d9afb18aa33c346918847b1b8d4ca774f359
Daniel Bacher <danielbacher@phill93.de>
2018-07-12 17:20:01 +0200

    add helper function for sorting rankings

---
+++ app/Helpers/SortRanking.php
@@ -0,0 +1,41 @@
+<?php
+
+if (!function_exists('SortRanking')) {
+
+    /**
+     * function for use with usort
+     * sort a array based on three values (wins, index, given_hits)
+     *
+     * @param array $a
+     * @param array $b
+     * @return int
+     */
+    function SortRanking(array $a, array $b)
+    {
+        if ($a['wins'] < $b['wins']) {
+            return -1;
+        }
+        if ($a['wins'] > $b['wins']) {
+            return 1;
+        }
+        if ($a['wins'] == $b['wins']) {
+            if ($a['index'] < $b['index']) {
+                return -1;
+            }
+            if ($a['index'] > $b['index']) {
+                return 1;
+            }
+            if ($a['index'] == $b['index']) {
+                if ($a['given_hits'] < $b['given_hits']) {
+                    return -1;
+                }
+                if ($a['given_hits'] > $b['given_hits']) {
+                    return 1;
+                }
+                if ($a['given_hits'] == $b['given_hits']) {
+                    return 0;
+                }
+            }
+        }
+    }
+}
